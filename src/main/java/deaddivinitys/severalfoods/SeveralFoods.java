1b4c46905b03d61532225ebddf756a1336d2946d
kevinkosterr <koster.k2001@gmail.com>
2021-07-10 14:34:02 +0200

    feat(meatballs): first implementation of meatballs.
    - textures for severalfoods:cooked_meatballs are still missing.
    - added meatball food items.
    - added crafting recipes and cooking.

--- src/main/java/deaddivinitys/severalfoods/SeveralFoods.java
+++ src/main/java/deaddivinitys/severalfoods/SeveralFoods.java
@@ -7,17 +7,20 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.item.Item;
 
 
+
 public class SeveralFoods implements ModInitializer {
 
 
-	public static final Item COOKED_MEATBALL = new Item(new Item.Settings().group(ItemGroup.FOOD));
-	public static final Item RAW_MEATBALL = new Item(new Item.Settings().group(ItemGroup.FOOD));
+	public static final Item COOKED_MEATBALL = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.COOKED_MEATBALL));
+	public static final Item RAW_MEATBALL = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.RAW_MEATBALL));
+	public static final Item COOKED_MEATBALLS = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.COOKED_MEATBALLS));
 
 	@Override
 	public void onInitialize() {
 
 		Registry.register(Registry.ITEM, new Identifier("severalfoods", "raw_meatball"), RAW_MEATBALL);
 		Registry.register(Registry.ITEM, new Identifier("severalfoods", "cooked_meatball"), COOKED_MEATBALL);
+		Registry.register(Registry.ITEM, new Identifier("severalfoods", "cooked_meatballs"), COOKED_MEATBALLS);
 
 	}
 }
---
+++ src/main/java/deaddivinitys/severalfoods/SeveralFoodsFoodComponents.java
@@ -0,0 +1,12 @@
+package deaddivinitys.severalfoods;
+
+import net.minecraft.item.FoodComponent;
+
+public class SeveralFoodsFoodComponents {
+
+    // Meatballs FoodComponents
+    public static final FoodComponent RAW_MEATBALL = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.2f).snack().build();
+    public static final FoodComponent COOKED_MEATBALL = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.5f).snack().build();
+    public static final FoodComponent COOKED_MEATBALLS = (new FoodComponent.Builder()).hunger(8).saturationModifier(0.8f).build();
+
+}

4b0d2d6947b4c69c12d85650f4d27701e787447d
kevinkosterr <koster.k2001@gmail.com>
2021-07-11 12:18:36 +0200

    feat(meatballs): textured meatball sandwich and added translation.
    also removed the cooked_meatballs from the game.

--- src/main/java/deaddivinitys/severalfoods/SeveralFoodsFoodComponents.java
+++ src/main/java/deaddivinitys/severalfoods/SeveralFoodsFoodComponents.java
@@ -7,7 +7,6 @@ public class SeveralFoodsFoodComponents {
     // Meatballs FoodComponents
     public static final FoodComponent RAW_MEATBALL = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.2f).snack().build();
     public static final FoodComponent COOKED_MEATBALL = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.5f).snack().build();
-    public static final FoodComponent COOKED_MEATBALLS = (new FoodComponent.Builder()).hunger(8).saturationModifier(0.8f).build();
 
     public static final FoodComponent UPSIDE_DOWN_BREAD = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.5f).build();
     public static final FoodComponent MEATBALL_SANDWICH = (new FoodComponent.Builder()).hunger(8).saturationModifier(0.8f).build();

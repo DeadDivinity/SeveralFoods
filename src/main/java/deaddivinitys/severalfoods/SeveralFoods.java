c17c7344556035ec25b9e3778f5dc81ba1143de3
kevinkosterr <koster.k2001@gmail.com>
2021-07-11 22:57:27 +0200

    feat(apples): created a notch apple and an emerald apple.
    also reformatted some code.

--- src/main/java/deaddivinitys/severalfoods/SeveralFoods.java
+++ src/main/java/deaddivinitys/severalfoods/SeveralFoods.java
@@ -16,13 +16,21 @@ public class SeveralFoods implements ModInitializer {
     public static final Item UPSIDE_DOWN_BREAD = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.UPSIDE_DOWN_BREAD));
     public static final Item MEATBALL_SANDWICH = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.MEATBALL_SANDWICH));
 
+    public static final Item ACTUAL_NOTCH_APPLE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.ACTUAL_NOTCH_APPLE));
+    public static final Item EMERALD_APPLE = new Item(new Item.Settings().group(ItemGroup.FOOD).food(SeveralFoodsFoodComponents.EMERALD_APPLE));
+
 
     @Override
     public void onInitialize() {
 
+        // Notch apple
+        Registry.register(Registry.ITEM, new Identifier("severalfoods", "actual_notch_apple"), ACTUAL_NOTCH_APPLE);
+        // Emerald apple
+        Registry.register(Registry.ITEM, new Identifier("severalfoods", "emerald_apple"), EMERALD_APPLE);
+
+        // Registry dedicated to the meatball-related items.
         Registry.register(Registry.ITEM, new Identifier("severalfoods", "raw_meatball"), RAW_MEATBALL);
         Registry.register(Registry.ITEM, new Identifier("severalfoods", "cooked_meatball"), COOKED_MEATBALL);
-        Registry.register(Registry.ITEM, new Identifier("severalfoods", "cooked_meatballs"), COOKED_MEATBALLS);
         Registry.register(Registry.ITEM, new Identifier("severalfoods", "upside_down_bread"), UPSIDE_DOWN_BREAD);
         Registry.register(Registry.ITEM, new Identifier("severalfoods", "meatball_sandwich"), MEATBALL_SANDWICH);
 
--- src/main/java/deaddivinitys/severalfoods/SeveralFoodsFoodComponents.java
+++ src/main/java/deaddivinitys/severalfoods/SeveralFoodsFoodComponents.java
@@ -1,14 +1,54 @@
 package deaddivinitys.severalfoods;
 
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class SeveralFoodsFoodComponents {
 
     // Meatballs FoodComponents
-    public static final FoodComponent RAW_MEATBALL = (new FoodComponent.Builder()).hunger(2).saturationModifier(0.2f).snack().build();
-    public static final FoodComponent COOKED_MEATBALL = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.5f).snack().build();
+    public static final FoodComponent RAW_MEATBALL = (new FoodComponent.Builder())
+            .hunger(1)
+            .saturationModifier(0.2f)
+            .snack()
+            .build();
 
-    public static final FoodComponent UPSIDE_DOWN_BREAD = (new FoodComponent.Builder()).hunger(6).saturationModifier(0.5f).build();
-    public static final FoodComponent MEATBALL_SANDWICH = (new FoodComponent.Builder()).hunger(8).saturationModifier(0.8f).build();
+    public static final FoodComponent COOKED_MEATBALL = (new FoodComponent.Builder())
+            .hunger(5)
+            .saturationModifier(0.5f)
+            .snack()
+            .build();
+
+    public static final FoodComponent UPSIDE_DOWN_BREAD = (new FoodComponent.Builder())
+            .hunger(6)
+            .saturationModifier(0.5f)
+            .build();
+
+    public static final FoodComponent MEATBALL_SANDWICH = (new FoodComponent.Builder())
+            .hunger(8)
+            .saturationModifier(0.8f)
+            .build();
+
+    // Notch Apple
+    public static final FoodComponent ACTUAL_NOTCH_APPLE = (new FoodComponent.Builder())
+            .hunger(20)
+            .saturationModifier(2.0f)
+            .alwaysEdible()
+            .snack()
+            .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 800, 4), 100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 800, 4), 100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 800, 4), 100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 800, 4), 100f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SATURATION, 800, 1), 100f)
+            .build();
+
+    // Emerald Apple
+    public static final FoodComponent EMERALD_APPLE = (new FoodComponent.Builder())
+            .hunger(15)
+            .saturationModifier(1.2f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.LUCK, 600, 2), 100f)
+            .build();
+
 
 }
